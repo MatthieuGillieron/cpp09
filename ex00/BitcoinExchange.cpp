29246c0c7561768f5db00ad61e5e657b9f7d685f
MatthieuGillieron <gillieron.matthieu@ik.me>
2026-03-13 15:47:15 +0100

    add: separate the keys

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -25,21 +25,25 @@ BitcoinExchange::~BitcoinExchange()
 
 void BitcoinExchange::loadDB(const std::string &file)
 {
-	if (!std::ifstream("data.csv"))
+	std::string sep = ",";
+	std::string buffer;
+	std::string value = "";
+
+	std::ifstream readFile(file);
+	if (!readFile)
 	{
 		std::cout << "Cannot open this file" << std::endl;
 		return;
 	}
-	std::string sep = ",";
-	std::string buffer;
 
-	std::ifstream readFile(file);
 	while (getline(readFile, buffer))
 	{
 		size_t pos = buffer.find(sep);
 		if (pos != std::string::npos)
 		{
-			std::cout << "trouver le sep" << sep << std::endl;
+			std:: string key = buffer.substr(0, pos);
+			std::cout << "trouver le sep" << key << std::endl;
+
 		}
 	}
 
