9692b290a7de736c1c972550be16325111723a29
MatthieuGillieron <gillieron.matthieu@ik.me>
2026-03-18 15:01:06 +0100

    fix: return error with cerr

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,10 +5,10 @@ int main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		std::cout << "exec: ./RPN <calcul>" << std::endl;
+		std::cerr << "exec: ./RPN <calcul>" << std::endl;
 		return 1;
 	}
-	
+
 	RPN rpn;
 	rpn.process(av[1]);
 }
