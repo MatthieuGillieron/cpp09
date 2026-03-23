7b46bcd67f7bba89d6edbbc4dbfafa64e677a54f
MatthieuGillieron <gillieron.matthieu@ik.me>
2026-03-23 14:59:34 +0100

    add: main class

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -3,19 +3,24 @@
 #define PMERGEME_HPP
 
 #include <string>
-#include <iostream>
-#include <algorithm>
+#include <vector>
+#include <deque>
 
 class PmergeMe
 {
 
 	private:
 
-
+		std::vector<int> _vec;
+		std::deque<int> _deq;
 
 	public:
 
-
+		void parseInput(int ac, char **av);
+		void sortVector();
+		void sortDeque();
+		void displayResult();
+		void displayTime();
 
 
 };
