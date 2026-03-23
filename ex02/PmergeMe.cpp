c61cd035103eda84f9582d5eb4de9cf9b2d1b283
MatthieuGillieron <gillieron.matthieu@ik.me>
2026-03-23 17:11:27 +0100

    add: methode for parse input

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -22,3 +22,35 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &other)
 PmergeMe::~PmergeMe()
 {}
 
+void PmergeMe::parseInput(int ac, char **av)
+{
+	if (ac < 2)
+	{
+		throw std::runtime_error("try: ./pmergeme <list_of_number>");
+		return;
+	}
+
+
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+		{
+			if (!isdigit(av[i][j]))
+			{
+				throw std::runtime_error("error: only digit is requiert");
+				return;
+			}
+		}
+
+		long val = std::strtol(av[i], NULL, 10);
+		if (val < 0 || val > INT_MAX)
+		{
+			throw std::runtime_error("error: range autorized: 0 - 2147483647");
+			return;
+		}
+		_vec.push_back(val);
+		_deq.push_back(val);
+		
+	}
+
+}
